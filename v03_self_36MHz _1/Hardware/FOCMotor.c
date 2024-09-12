91cb7df4be23b518f208215d93a5f584419014ae
zhouhang <2638566031@qq.com>
2024-09-12 12:09:04 +0800

    v0.2 @36MHz

---
+++ v03_self_36MHz _1/Hardware/FOCMotor.c
@@ -0,0 +1,13 @@
+#include "Myproject.h"
+
+float shaft_angle; //»úÐµ½Ç¶È
+float electrical_angle;
+float shaft_velocity;
+float current_sp;
+float shaft_velocity_sp;
+float shaft_angle_sp;
+DQVoltage_s voltage;
+DQCurrent_s current;
+
+TorqueControlType torque_controller;
+MotionControlType controller;
---
+++ v03_self_36MHz _1/Hardware/FOCMotor.h
@@ -0,0 +1,20 @@
+#ifndef FOCMotor_H
+#define FOCMotor_H
+
+typedef enum
+{
+	Type_torque,//!< Torque control
+	Type_velocity,//!< Velocity motion control
+	Type_angle,//!< Position/angle motion control
+	Type_velocity_openloop,
+	Type_angle_openloop
+} MotionControlType;
+
+typedef enum
+{
+	Type_voltage, //!< Torque control using voltage
+	Type_dc_current, //!< Torque control using DC current (one current magnitude)
+	Type_foc_current //!< torque control using dq currents
+} TorqueControlType;
+
+#endif
